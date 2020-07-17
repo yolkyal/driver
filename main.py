7a75b683e2eee6819f9046a9136b936d180b2252
harrisonjones <harry.jones@loveholidays.com>
2020-07-17 15:40:28 +0100

    Add background image, improvements to consts

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
 
 	car_id = 'CAR_ID'
 	wheel_id = 'WHEEL_ID'
+	lot_id = 'LOT_ID'
 	car_start_pos = (400, 600)
 	car_length = 90
 	car_width = 50
@@ -28,6 +29,7 @@ def main():
 	image_manager = ImageManager()
 	image_manager.put(car_id, 'images/car.png', (64, 112))
 	image_manager.put(wheel_id, 'images/tyre.png', (8, 24))
+	image_manager.put(lot_id, 'images/lot.png', (800, 800))
 
 	car = Car(car_id, wheel_id, car_start_pos, car_length, car_width, car_angle, wheel_angle)
 	car_controller = CarController(car)
@@ -45,7 +47,7 @@ def main():
 		car_controller.update()
 		car.update()
 
-		d_surf.fill(BG_COL)
+		d_surf.blit(image_manager.get(lot_id), (0, 0))
 		car_drawer.draw(d_surf, car)
 		pygame.display.update()
 	
